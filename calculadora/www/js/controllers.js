d8dd4c39c6a1d8c1cf6c3c1319b238317d2848a8
Cristian Cortez <cortez.cristian@gmail.com>
2016-09-02 20:22:22 -0300

    Calculator

---
+++ calculadora/www/js/controllers.js
@@ -0,0 +1,40 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, LocalStorage, Calculator) {
+  $scope.result = '';
+
+  //LocalStorage.set("demo", {hola:"mundo"});
+
+  $scope.calc = function(n){
+    if(n === '=') {
+      $scope.result = Calculator.evaluate($scope.result);
+    } else {
+      $scope.result += (n == 'x') ? '*' : n;
+    }
+  }
+})
+
+.controller('ChatsCtrl', function($scope, Chats, LocalStorage) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
---
+++ calculadora/www/js/services.js
@@ -0,0 +1,73 @@
+angular.module('starter.services', [])
+
+.factory('LocalStorage', function() {
+  return {
+    get: function(key){
+      return JSON.parse(localStorage[key]);
+    },
+    set: function(key, val){
+      localStorage.setItem(key, JSON.stringify(val));
+      return JSON.parse(localStorage[key]);
+    },
+    remove: function(key){
+      localStorage.removeItem(key);
+    }
+  };
+})
+
+.factory('Calculator', function() {
+  return {
+    evaluate: function(calc){
+      return eval(calc);
+    }
+  };
+})
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
